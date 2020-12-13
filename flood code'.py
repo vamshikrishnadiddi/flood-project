eb8a1dd9fdfaa27827fc304dd70e5defcbb34c8a
vamshi <dvamshi2204@gmail.com>
2020-12-13 18:37:48 +0530

    commiting in 13122020

--- flood code'.py
+++ flood code'.py
@@ -18,6 +18,7 @@ from sklearn.preprocessing import StandardScaler
 sc = StandardScaler()
 X_train = sc.fit_transform(X_train)
 X_test = sc.transform(X_test)
+changed
 
 from sklearn.svm import SVC
 classifier = SVC(kernel = 'rbf', random_state = 0)
